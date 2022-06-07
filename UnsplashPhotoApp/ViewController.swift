84aa17a0313137c35c63e8c0e11ba29461cafde0
Vladimir <tekmya@mail.ru>
2022-06-07 23:17:06 +0300

    Initial Commit

---
+++ UnsplashPhotoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UnsplashPhotoApp
+//
+//  Created by Владимир Ли on 07.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
